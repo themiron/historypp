0ac4d9bbe6f597a5aef3c8e5d295c1c2b8cfe299
oxygen <oxygen@6986caf6-1fe9-11df-980b-c1ecf858a722>
2006-03-30 07:57:13 +0000

    Added make scripts and necessary code modifications

--- historypp/trunk/build.bat
+++ historypp/trunk/build.bat
@@ -2,7 +2,8 @@
 
 cd plugin
 
-brcc32 hpp_resource.rc
+brcc32 -fohpp_resource.res hpp_resource.rc
+brcc32 -fohpp_res_ver.res hpp_res_ver.rc
 
 rem #
 rem # Find tntControls path relatively to our current dir
@@ -18,17 +19,17 @@ set TRIES=0%TRIES%
 goto loop
 :exitloop
 
-set DELPHILIB=c:\program files\borland\bds\4.0\lib;c:\program files\borland\delphi7\lib
+set DELPHILIB=c:\program files\borland\bds\4.0\lib;c:\program files\borland\delphi7\lib;c:\program files\borland\bds\2.0\lib;c:\program files\borland\bds\3.0\lib
 set INCDIR="%DELPHILIB%;%TNTPATH%;..\inc;"
 set OUTDIR=".."
 set DCUDIR="tmp"
 : A4 Aligned record fields  
 : D Debug information          
-: J  Writeable structured consts (need to compile)
+: J+  Writeable structured consts (NEED ON)
 : L  Local debug symbols 
 : O  Optimization 
 : Q  Integer overflow checking
-: R  Range checking 
+: R- Range checking (NEED OFF?)
 : Y  Symbol reference info
 set COMPDIR=-$A4 -$D- -$J+ -$L- -$O+ -$Q+ -$R- -$Y-
 
--- historypp/trunk/plugin/hpp_global.pas
+++ historypp/trunk/plugin/hpp_global.pas
@@ -58,10 +58,10 @@ const
 
   hppName       = 'History++';
   hppDBName     = 'HistoryPlusPlus';
-  hppVerMajor   = 1;
-  hppVerMinor   = 5;
-  hppVerRelease = 0;
-  hppVerBuild   = 99;
+  hppVerMajor   = {MAJOR_VER}1{/MAJOR_VER};
+  hppVerMinor   = {MINOR_VER}5{/MINOR_VER};
+  hppVerRelease = {SUB_VER}0{/SUB_VER};
+  hppVerBuild   = {BUILD}99{/BUILD};
   hppVersion    = hppVerMajor shl 24 + hppVerMinor shl 16 + hppVerRelease shl 8 + hppVerBuild;
 
   hppUpdateURL = 'http://slav.pp.ru/miranda/historypp.zip';
