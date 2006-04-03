7835c148d12969fcde8749d16e51d1209e51723a
oxygen <oxygen@6986caf6-1fe9-11df-980b-c1ecf858a722>
2006-04-03 07:03:43 +0000

    Update translation tools

--- historypp/trunk/build_scripts/make_translation.bat
+++ historypp/trunk/build_scripts/make_translation.bat
@@ -1,6 +1,29 @@
 @echo off
 
-copy ..\plugin\*.trans.txt hpp_t.txt
+rem copy ..\plugin\*.trans.txt hpp_t.txt
+
+set TRANS=..\build_scripts\hpp_t.txt
+
+call change_vars.bat r+ trans_header.txt hpp_t.txt
+
+cd ..\plugin
+
+rem echo ;;>%TRANS%
+rem echo ;; History++ plugin translation>> %TRANS%
+rem echo ;; For version %VER%>> %TRANS%
+rem echo ;; >>%TRANS%
+rem echo ;; Contact theMIROn (themiron@mail.ru), Art Fedorov (artemf@mail.ru)>>%TRANS%
+rem echo ;; >>%TRANS%
+FOR %%A IN (*.trans.txt) DO (
+echo:>>%TRANS%
+echo:>>%TRANS%
+rem echo ;;>> %TRANS%
+echo ;; %%A file >> %TRANS%
+echo:>>%TRANS%
+rem echo ;;>> %TRANS%
+type %%A >> %TRANS%
+)
+cd ..\build_scripts
 
 rem #
 rem # Find Utils path relatively to our current dir
@@ -18,5 +41,11 @@ goto loop
 
 set SED=%UTILSPATH%\sed.exe
 
-%SED% --text -f rem_dupes.sed hpp_t.txt > hpp_trans.txt
-del hpp_t.txt
+%SED% --text -f rem_dupes.sed hpp_t.txt > hpp_tmp.txt
+move hpp_tmp.txt hpp_t.txt
+%SED% --text -f rem_doubles.sed hpp_t.txt > hpp_tmp.txt
+move hpp_tmp.txt hpp_t.txt
+%SED% --text -f enclose.sed hpp_t.txt > hpp_tmp.txt
+move hpp_tmp.txt hpp_t.txt
+
+move hpp_t.txt hpp_trans.txt
