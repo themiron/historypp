3a22f88ae2384be002dad9f394ba7b48926162d3
oxygen <oxygen@6986caf6-1fe9-11df-980b-c1ecf858a722>
2006-05-24 21:48:11 +0000

    Bookmarks update

--- historypp/trunk/plugin/hpp_bookmarks.pas
+++ historypp/trunk/plugin/hpp_bookmarks.pas
@@ -44,14 +44,20 @@ type
   private
     Bookmarks: TBookmarksHash;
     hContact: THandle;
+    FContactCP: Cardinal;
     function GetBookmarked(Index: THandle): Boolean;
     procedure SetBookmarked(Index: THandle; const Value: Boolean);
+
+    procedure DeleteBookmarks;
+    procedure LoadBookmarks;
+    procedure SaveBookmarks;
   public
     constructor Create(AContact: THandle);
     destructor Destroy; override;
 
     property Bookmarked[Index: THandle]: Boolean read GetBookmarked write SetBookmarked;
     property Contact: THandle read hContact;
+    property ContactCP: Cardinal read FContactCP;
   end;
   PContactBookmarks = ^TContactBookmarks;
 
@@ -67,7 +73,7 @@ type
     procedure RemoveByIndex(Index: Integer);
     procedure InsertByIndex(Index: Integer; Key,Value: Cardinal);
   protected
-    procedure AddKey(Key, Value: Cardinal);
+    function AddKey(Key, Value: Cardinal): Boolean;
     function GetKey(Key: Cardinal; var Value: Cardinal): Boolean;
     function RemoveKey(Key: Cardinal): Boolean;
   public
@@ -90,10 +96,12 @@ type
     function GetBookmark(hDBEvent: THandle; var EventData: TEventData): Boolean;
     function AddItem(hDBEvent: THandle): Boolean;
     function RemoveItem(hDBEvent: THandle): Boolean;
+    function FindEventByTimestampAndCrc(ped: PEventData): Boolean;
   public
     constructor Create(AContact: TContactBookmarks);
     destructor Destroy; override;
 
+    function AddEventData(var EventData: TEventData): Boolean;
     property HasItem[Index: THandle]: Boolean read GetHasItem; default;
   end;
 
@@ -125,6 +133,8 @@ procedure hppDeinitBookmarkServer;
 
 implementation
 
+uses hpp_events, hpp_contacts, hpp_global, Checksum, hpp_database;
+
 procedure hppInitBookmarkServer;
 begin
   BookmarkServer := TBookmarkServer.Create;
@@ -153,12 +163,7 @@ end;
 
 function DynArrayComparePseudoHash(Item1, Item2: Pointer): Integer;
 begin
-  if PPseudoHashEntry(Item1)^.Key > PPseudoHashEntry(Item2)^.Key then
-    Result := 1
-  else if PPseudoHashEntry(Item1)^.Key < PPseudoHashEntry(Item2)^.Key then
-    Result := -1
-  else
-    Result := 0;
+  Result := PInteger(@PPseudoHashEntry(Item1)^.Key)^ - PInteger(@PPseudoHashEntry(Item2)^.Key)^;
 end;
 
 { TBookmarkServer }
@@ -226,8 +231,15 @@ end;
 constructor TContactBookmarks.Create(AContact: THandle);
 begin
   hContact := AContact;
+  FContactCP := GetContactCodepage(hContact);
   Bookmarks := TBookmarksHash.Create(Self);
   // read bookmarks from DB here
+  LoadBookmarks;
+end;
+
+procedure TContactBookmarks.DeleteBookmarks;
+begin
+  DBDelete(hContact,hppDBName,'Bookmarks');
 end;
 
 destructor TContactBookmarks.Destroy;
@@ -241,30 +253,87 @@ begin
   Result := Bookmarks[Index];
 end;
 
-procedure TContactBookmarks.SetBookmarked(Index: THandle; const Value: Boolean);
+procedure TContactBookmarks.LoadBookmarks;
+var
+  i: Integer;
+  mem: PEventData;
+  mem_org: Pointer;
+  mem_len: Integer;
+  rec_size: Word;
+  count: Integer;
+  ed: PEventData;
+  AllOk: Boolean;
 begin
-  if Value <> Bookmarks[Index] then begin
-    if Value then
-      Bookmarks.AddItem(Index)
-    else
-      Bookmarks.RemoveItem(Index);
+  if not GetDBBlob(hContact,hppDBName,'Bookmarks',mem_org,mem_len) then exit;
+  try
+    AllOk := True;
+    if mem_len < SizeOf(Word) then raise EAbort.Create('Too small bookmarks rec');
+    rec_size := PWord(mem_org)^;
+    if rec_size < SizeOf(TEventData) then raise EAbort.Create('Bookmark size is too small');
+    count := (mem_len - SizeOf(Word)) div rec_size;
+    mem := Pointer(DWord(mem_org) + Sizeof(Word));
+    for i := 0 to count - 1 do begin
+      ed := PEventData(Integer(mem)+i*rec_size);
+      if not Bookmarks.AddEventData(ed^) then AllOk := False;
+    end;
+    FreeMem(mem_org,mem_len);
+    // if we found that some items are missing or different, save
+    // correct copy:
+    if not AllOk then SaveBookmarks;
+  except
+    DeleteBookmarks;
   end;
+end;
+
+procedure TContactBookmarks.SaveBookmarks;
+var
+  mem: Pointer;
+  mem_len: Integer;
+  i: Integer;
+  src,dst: PEventData;
+begin
+  if Length(Bookmarks.Table) > 0 then begin
+    mem_len := Length(Bookmarks.Table)*SizeOf(TEventData)+SizeOf(Word);
+    GetMem(mem,mem_len);
+    PWord(mem)^ := Word(SizeOf(TEventData));
+    for i := 0 to High(Bookmarks.Table) do begin
+      src := PEventData(Bookmarks.Table[i].Value);
+      dst := PEventData(Integer(mem)+SizeOf(Word)+i*SizeOf(TEventData));
+      Move(src^,dst^,SizeOf(src^));
+    end;
+    WriteDBBlob(hContact,hppDBName,'Bookmarks',mem,mem_len);
+  end
+  else begin
+    DeleteBookmarks;
+  end;
+end;
+
+procedure TContactBookmarks.SetBookmarked(Index: THandle; const Value: Boolean);
+var
+  res: Boolean;
+begin
+  if Value then
+    res := Bookmarks.AddItem(Index)
+  else
+    res := Bookmarks.RemoveItem(Index);
+  if res then SaveBookmarks;
 end;
 
 
 { TPseudoHash }
 
-procedure TPseudoHash.AddKey(Key, Value: Cardinal);
+function TPseudoHash.AddKey(Key, Value: Cardinal): Boolean;
 var
   Nearest: Integer;
   ph: TPseudoHashEntry;
   i: Integer;
 begin
+  Result := False;
   ph.Key := Key;
   Nearest := SearchDynArray(Table,SizeOf(TPseudoHashEntry),DynArrayComparePseudoHash,@ph,True);
   if Nearest <> -1 then begin // we have nearest or match
     if Table[Nearest].Key = Key then
-      raise Exception.Create('TPseudoHash: Key already exists');
+      exit;
     if Table[Nearest].Key < Key then
       Inc(Nearest);
   end
@@ -277,6 +346,8 @@ begin
 
   Table[Nearest].Key := Key;
   Table[Nearest].Value := Value;
+
+  Result := True;
 end;
 
 destructor TPseudoHash.Destroy;
@@ -350,13 +421,49 @@ end;
 
 { TBookmarksHash }
 
+function TBookmarksHash.AddEventData(var EventData: TEventData): Boolean;
+var
+  ped: PEventData;
+  hi: THistoryItem;
+  ts: Cardinal;
+  ItemExists, ItemCorrect, NewItemFound: Boolean;
+begin
+  GetMem(ped,SizeOf(TEventData));
+  ped^.hDBEvent := EventData.hDBEvent;
+  ped^.CRC32 := EventData.CRC32;
+  ped^.Timestamp := EventData.Timestamp;
+  ItemExists := (PluginLink.CallService(MS_DB_EVENT_GETBLOBSIZE,EventData.hDBEvent,0) >= 0);
+  if ItemExists then begin
+    ts := GetEventTimestamp(EventData.hDBEvent);
+    ItemCorrect := (ts = ped^.Timestamp);
+    // we might check for CRC32 here also?
+  end;
+  if (not ItemExists) or (not ItemCorrect) then begin
+    Result := False;
+    NewItemFound := FindEventByTimestampAndCrc(ped); // try to find the item
+    if not NewItemFound then begin // can not find
+      FreeMem(ped,SizeOf(TEventData));
+      exit;
+    end
+    else
+      AddKey(ped^.hDBEvent,Cardinal(ped)); // exit, but leave Result = False as we want to resave after this load
+  end
+  else
+    Result := AddKey(ped^.hDBEvent,Cardinal(ped)); // item exists, add as normal
+end;
+
 function TBookmarksHash.AddItem(hDBEvent: THandle): Boolean;
 var
   ped: PEventData;
+  hi: THistoryItem;
 begin
   GetMem(ped,SizeOf(TEventData));
   ped^.hDBEvent := hDBEvent;
+  hi := ReadEvent(hDBEvent,Contact.ContactCP);
+  ped^.Timestamp := hi.Time;
+  CalcCRC32(PWideChar(hi.Text),Length(hi.Text)*SizeOf(WideChar),Cardinal(ped^.CRC32));
   AddKey(hDBEvent,Cardinal(ped));
+  Result := True;
 end;
 
 constructor TBookmarksHash.Create(AContact: TContactBookmarks);
@@ -371,6 +478,53 @@ begin
   for i := 0 to Length(Table) - 1 do
     FreeMem(PEventData(Table[i].Value),SizeOf(TEventData));
   inherited;
+end;
+
+// currently finds events with similar timestamp ONLY
+function TBookmarksHash.FindEventByTimestampAndCrc(ped: PEventData): Boolean;
+var
+  hDBEvent: THandle;
+  first_ts,last_ts,ts,cur_ts: Integer;
+  StartFromFirst: Boolean;
+begin
+  Result := False;
+
+  hDBEvent := PluginLink.CallService(MS_DB_EVENT_FINDFIRST,Contact.hContact,0);
+  if hDBEvent = 0 then exit;
+  first_ts := GetEventTimestamp(hDBEvent);
+  hDBEvent := PluginLink.CallService(MS_DB_EVENT_FINDLAST,Contact.hContact,0);
+  if hDBEvent = 0 then exit;
+  last_ts := GetEventTimestamp(hDBEvent);
+  ts := ped^.Timestamp;
+  if (ts < first_ts) or (ts > last_ts) then exit;
+  StartFromFirst := ((ts - first_ts) < (last_ts - ts));
+
+  if StartFromFirst then begin
+    hDBEvent := PluginLink.CallService(MS_DB_EVENT_FINDFIRST,Contact.hContact,0);
+    while hDBEvent <> 0 do begin
+      cur_ts := GetEventTimestamp(hDBEvent);
+      if cur_ts > ts then break;
+      if cur_ts = ts then begin
+        ped^.hDBEvent := hDBEvent;
+        Result := True;
+        break;
+      end;
+      hDBEvent := PluginLink.CallService(MS_DB_EVENT_FINDNEXT,hDBEvent,0);
+    end;
+  end
+  else begin
+    hDBEvent := PluginLink.CallService(MS_DB_EVENT_FINDLAST,Contact.hContact,0);
+    while hDBEvent <> 0 do begin
+      cur_ts := GetEventTimestamp(hDBEvent);
+      if ts > cur_ts then break;
+      if cur_ts = ts then begin
+        ped^.hDBEvent := hDBEvent;
+        Result := True;
+        break;
+      end;
+      hDBEvent := PluginLink.CallService(MS_DB_EVENT_FINDPREV,hDBEvent,0);
+    end;
+  end;
 end;
 
 function TBookmarksHash.GetBookmark(hDBEvent: THandle;
