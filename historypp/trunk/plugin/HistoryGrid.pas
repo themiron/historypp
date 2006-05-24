3e84f0f4e9cb8f0380d908c39ac8ef50fca2f87d
Vladislav Grishenko <themiron@mail.ru>
2006-05-24 18:05:59 +0000

    Fixed multiple first event selection causing bottom align loose

--- historypp/trunk/plugin/HistoryGrid.pas
+++ historypp/trunk/plugin/HistoryGrid.pas
@@ -4150,15 +4150,18 @@ begin
           FSelected := GetDown(FSelected);
           if FSelected = -1 then
             FSelected := GetUp(FSelected);
-          Selected := FSelected;
+          // we have multiple selection sets
+          //Selected := FSelected;
         end
         else begin
           FSelected := 0;
           SetSBPos(GetIdx(0));
           if Reversed then
-            Selected := GetPrev(-1)
+            // we have multiple selection sets
+            FSelected := GetPrev(-1)
           else
-            Selected := GetNext(-1);
+            // we have multiple selection sets
+            FSelected := GetNext(-1);
         end;
       finally
         ShowProgress := False;
