5917b604e731b6fcc73815cf720bc47a35b0393b
oxygen <oxygen@6986caf6-1fe9-11df-980b-c1ecf858a722>
2006-05-24 12:29:13 +0000

    Add hash classes to bookmarks

--- historypp/trunk/plugin/hpp_bookmarks.pas
+++ historypp/trunk/plugin/hpp_bookmarks.pas
@@ -28,7 +28,7 @@ unit hpp_bookmarks;
 
 interface
 
-uses m_globaldefs, m_api;
+uses m_globaldefs, m_api, hpp_jclSysUtils, SysUtils;
 
 type
   TEventData = packed record
@@ -38,9 +38,42 @@ type
     Position: DWord;
   end;
 
+  TContactBookmarks = class(TObject)
+  private
+    Bookmarks: array of TEventData;
+    hContact: THandle;
+  public
+    constructor Create(AContact: THandle);
+    destructor Destroy; override;
+
+    property Contact: THandle read hContact;
+  end;
+  PContactBookmarks = ^TContactBookmarks;
+
+  TPseudoHashEntry = record
+    Key: Cardinal;
+    Value: Cardinal;
+  end;
+  PPseudoHashEntry = ^TPseudoHashEntry;
+
+  TPseudoHash = class(TObject)
+  private
+    Table: array of TPseudoHashEntry;
+    procedure AddKey(Key, Value: Cardinal);
+    function GetKey(Key: Cardinal; var Value: Cardinal): Boolean;
+  end;
+
+  TContactsHash = class(TPseudoHash)
+  private
+    function GetContactBookmarks(Index: Integer): TContactBookmarks;
+  public
+    property Items[Index: THandle]: TContactBookmarks read GetContactBookmarks;
+  end;
+
   TBookmarkServer = class(TObject)
   private
     hookEventDeleted,hookEventAdded: THandle;
+    Contacts: TContactsHash;
   protected
     procedure EventDeleted(hContact,hDBEvent: THandle);
     procedure EventAdded(hContact,hDBEvent: THandle);
@@ -139,6 +172,86 @@ end;
 procedure TBookmarkServer.EventDeleted(hContact, hDBEvent: THandle);
 begin
   DeleteBookmark(hContact, hDBEvent);
+end;
+
+{ TContactBookmarks }
+
+constructor TContactBookmarks.Create(AContact: THandle);
+begin
+  hContact := AContact;
+  // read bookmarks from DB here
+end;
+
+destructor TContactBookmarks.Destroy;
+begin
+  SetLength(Bookmarks,0);
+  inherited;
+end;
+
+{ TContactsHash }
+
+
+{ TPseudoHash }
+
+function DynArrayComparePseudoHash(Item1, Item2: Pointer): Integer;
+begin
+  Result := PPseudoHashEntry(Item1)^.Key - PPseudoHashEntry(Item2)^.Key;
+end;
+
+procedure TPseudoHash.AddKey(Key, Value: Cardinal);
+var
+  Nearest: Integer;
+  ph: TPseudoHashEntry;
+  i: Integer;
+begin
+  ph.Key := Key;
+  Nearest := SearchDynArray(Table,SizeOf(TPseudoHashEntry),DynArrayComparePseudoHash,
+    @ph,True);
+  if Nearest <> -1 then begin // we have nearest or match
+    if Table[Nearest].Key = Key then
+      raise Exception.Create('TPseudoHash: Key already exists');
+    if Table[Nearest].Key < Key then
+      Inc(Nearest);
+  end
+  else
+    Nearest := 0; // table is empty
+
+  SetLength(Table,Length(Table)+1);
+  for i := Length(Table)-1 downto Nearest do
+    Table[i] := Table[i-1];
+
+  Table[Nearest].Key := Key;
+  Table[Nearest].Value := Value;
+end;
+
+function TPseudoHash.GetKey(Key: Cardinal; var Value: Cardinal): Boolean;
+var
+  ph: TPseudoHashEntry;
+  res: Integer;
+begin
+  Result := False;
+  ph.Key := Key;
+  res := SearchDynArray(Table,SizeOf(TPseudoHashEntry),DynArrayComparePseudoHash,
+    @ph,False);
+  if res <> -1 then begin
+    Result := True;
+    Value := Table[res].Value;
+  end;
+end;
+
+{ TContactsHash }
+
+function TContactsHash.GetContactBookmarks(Index: Integer): TContactBookmarks;
+var
+  val: Pointer;
+begin
+  Result := nil;
+  if GetKey(Cardinal(Index),Cardinal(val)) then
+    Result := PContactBookmarks(val)^
+  else begin
+    Result := TContactBookmarks.Create(Index);
+    AddKey(Cardinal(Index),Cardinal(PContactBookmarks(Result)));
+  end;
 end;
 
 end.
