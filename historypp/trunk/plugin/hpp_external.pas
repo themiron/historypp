0b264e21b88b5de370498a8276daccb4eaa22b1d
oxygen <artemf@mail.ru>
2006-09-26 16:12:29 +0000

    Rough and preliminary support for external grid, imitates ieview for now if built with IMITATE_IEVIEW define, works with tabsrmm

---
+++ historypp/trunk/plugin/hpp_external.pas
@@ -0,0 +1,184 @@
+unit hpp_external;
+
+interface
+
+uses
+  Windows, HistoryGrid, m_globaldefs, m_api;
+
+type
+
+  IEVIEWWINDOW = packed record
+    cbSize: Integer;  // size of the strusture
+    iType: Integer;   // one of IEW_* values
+    dwMode: DWord;    // compatibility mode - one of IEWM_* values
+    dwFlags: DWord;   // flags, one of IEWF_* values
+    Parent: HWND;     // parent window HWND
+    Hwnd: HWND;       // IEW_CREATE returns WebBrowser control's HWND here
+    x: Integer;       // IE control horizontal position
+    y: Integer;       // IE control vertical position
+    cx: Integer;      // IE control horizontal size
+    cy: Integer;      // IE control vertical size
+  end;
+
+  PIEVIEWWINDOW = ^IEVIEWWINDOW;
+
+  IEVIEWEVENT = record
+    cbSize: Integer;        // size of the strusture
+    iType: Integer;         // one of IEE_* values
+    dwFlags: DWord;         // one of IEEF_* values
+    Hwnd: HWND;             // HWND returned by IEW_CREATE
+    hContact: THandle;      // contact
+    hDBEventFirst: THandle; // first event to log, when IEE_LOG_EVENTS returns it will contain
+                            // the last event actually logged or NULL if no event was logged (IEE_LOG_EVENTS)
+    Count: Integer;         // number of events to log
+    Codepage: Integer;      // ANSI codepage
+    pszProto: PChar;        // Name of the protocol
+  end;
+
+  PIEVIEWEVENT = ^IEVIEWEVENT;
+  
+  (*typedef struct {
+	union {
+		HANDLE 		hDbEventFirst;      /
+
+		IEVIEWEVENTDATA *eventData;	    // the pointer to an array of IEVIEWEVENTDATA objects (IEE_LOG_IEV_EVENTS)
+	};
+} IEVIEWEVENT; *)
+const
+  IEW_CREATE       = 1; // create new window (control)
+  IEW_DESTROY      = 2; // destroy control
+  IEW_SETPOS       = 3; // set window position and size
+  IEW_SCROLLBOTTOM = 4; // scroll text to bottom
+
+  IEE_LOG_DB_EVENTS  = 1; // log specified number of DB events
+  IEE_CLEAR_LOG	     = 2; // clear log
+  IEE_GET_SELECTION	 = 3; // get selected text
+  IEE_SAVE_DOCUMENT	 = 4; // save current document
+  IEE_LOG_MEM_EVENTS = 5; // log specified number of IEView events
+
+  IEEF_RTL          = 1; // turn on RTL support
+  IEEF_NO_UNICODE   = 2; // disable Unicode support
+  IEEF_NO_SCROLLING = 4; // do not scroll logs to bottom
+  
+const
+  MS_IEVIEW_WINDOW = 'IEVIEW/NewWindow';
+  MS_IEVIEW_EVENT	 = 'IEVIEW/Event';
+  MS_IEVIEW_UTILS  = 'IEVIEW/Utils';
+
+  ME_IEVIEW_OPTIONSCHANGED = 'IEVIEW/OptionsChanged';
+  ME_IEVIEW_NOTIFICATION   = 'IEVIEW/Notification';
+
+var
+  hExtWindow, hExtEvent, hExtUtils: THandle;
+  hExtOptChanged, hExtNotification: THandle;
+
+procedure RegisterExtGridServices;
+procedure UnregisterExtGridServices;
+
+implementation
+
+uses hpp_externalgrid;
+
+function ExtWindow(wParam, lParam: DWord): Integer; cdecl;
+var
+  par: PIEVIEWWINDOW;
+  i,n: Integer;
+begin
+  try
+    par := PIEVIEWWINDOW(lParam);
+    if par.iType = IEW_CREATE then begin
+      OutputDebugString('IEW_CREATE');
+      n := Length(ExternalGrids);
+      SetLength(ExternalGrids,n+1);
+      ExternalGrids[n] := TExternalGrid.Create(par.Parent);
+      par.Hwnd := n;
+    end
+    else if par.iType = IEW_DESTROY then begin
+      OutputDebugString('IEW_DESTROY');
+      n := par.Hwnd;
+      ExternalGrids[n].Free;
+      for i := n to Length(ExternalGrids) - 2 do
+        ExternalGrids[i] := ExternalGrids[i+1];
+      SetLength(ExternalGrids,Length(ExternalGrids)-1);
+    end
+    else if par.iType = IEW_SETPOS then begin
+      OutputDebugString('IEW_SETPOS');
+      n := par.Hwnd;
+      ExternalGrids[n].SetPosition(par.x,par.y,par.cx,par.cy);
+    end
+    else if par.iType = IEW_SCROLLBOTTOM then begin
+      OutputDebugString('IEW_SCROLLBOTTOM');
+      n := par.Hwnd;
+      ExternalGrids[n].ScrollToBottom;
+    end;
+    Result := 0;
+  except
+    Result := 1;
+  end;
+end;
+
+function ExtEvent(wParam, lParam: DWord): Integer; cdecl;
+var
+  event: PIEVIEWEVENT;
+  hDBNext: THandle;
+  n: Integer;
+begin
+  try
+    OutputDebugString('MS_IEVIEW_EVENT');
+    event := PIEVIEWEVENT(lParam);
+    if event.iType = IEE_LOG_DB_EVENTS then begin
+      n := event.Hwnd;
+      ExternalGrids[n].AddEvent(event.hContact, event.hDBEventFirst, event.Codepage);
+      if event.Count = -1 then begin
+        hDBNext := PluginLink.CallService(MS_DB_EVENT_FINDNEXT,event.hDBEventFirst,0);
+        while hDBNext <> 0 do begin
+          ExternalGrids[n].AddEvent(event.hContact, hDBNext, event.Codepage);
+          hDBNext := PluginLink.CallService(MS_DB_EVENT_FINDNEXT,hDBNext,0);
+        end;
+      end;
+    end
+    else if event.iType = IEE_CLEAR_LOG then begin
+
+    end
+    else if event.iType = IEE_GET_SELECTION then begin
+      
+    end;
+    Result := 0;
+  except
+    Result := 1;
+  end;
+end;
+
+function ExtUtils(wParam, lParam: DWord): Integer; cdecl;
+begin
+  try
+    OutputDebugString('MS_IEVIEW_UTILS');
+    Result := 0;
+  except
+    Result := 1;
+  end;
+end;
+
+procedure RegisterExtGridServices;
+begin
+  {$IFDEF IMITATE_IEVIEW}
+  hExtWindow := PluginLink.CreateServiceFunction(MS_IEVIEW_WINDOW,ExtWindow);
+  hExtEvent := PluginLink.CreateServiceFunction(MS_IEVIEW_EVENT,ExtEvent);
+  hExtUtils := PluginLink.CreateServiceFunction(MS_IEVIEW_UTILS,ExtUtils);
+  hExtOptChanged := PluginLink.CreateHookableEvent(ME_IEVIEW_OPTIONSCHANGED);
+  hExtNotification := PluginLink.CreateHookableEvent(ME_IEVIEW_NOTIFICATION);
+  {$ENDIF}
+end;
+
+procedure UnregisterExtGridServices;
+begin
+  {$IFDEF IMITATE_IEVIEW}
+  PluginLink.DestroyServiceFunction(hExtWindow);
+  PluginLink.DestroyServiceFunction(hExtEvent);
+  PluginLink.DestroyServiceFunction(hExtUtils);
+  PluginLink.DestroyHookableEvent(hExtOptChanged);
+  PluginLink.DestroyHookableEvent(hExtNotification);
+  {$ENDIF}
+end;
+
+end.
---
+++ historypp/trunk/plugin/hpp_externalgrid.pas
@@ -0,0 +1,104 @@
+unit hpp_externalgrid;
+
+interface
+
+uses
+  Windows, hpp_global, m_globaldefs, m_api, hpp_events, HistoryGrid;
+
+type
+  TExtItem = record
+    hDBEvent: THandle;
+    hContact: THandle;
+    Codepage: THandle;
+  end;
+
+  TExternalGrid = class(TObject)
+  private
+    Items: array of TExtItem;
+    Grid: THistoryGrid;
+    FParentWindow: HWND;
+    function GetGridHandle: HWND;
+  protected
+    procedure GridItemData(Sender: TObject; Index: Integer; var Item: THistoryItem);
+    procedure GridTranslateTime(Sender: TObject; Time: Cardinal; var Text: WideString);
+  public
+    constructor Create(AParentWindow: HWND);
+    destructor Destroy; override;
+
+    procedure AddEvent(hContact, hDBEvent: THandle; Codepage: Integer);
+    procedure SetPosition(x,y,cx,cy: Integer);
+    procedure ScrollToBottom;
+    property ParentWindow: HWND read FParentWindow;
+    property GridHandle: HWND read GetGridHandle;
+  end;
+
+var
+  ExternalGrids: array of TExternalGrid;
+
+implementation
+
+uses hpp_options;
+
+{ TExternalGrid }
+
+procedure TExternalGrid.AddEvent(hContact, hDBEvent: THandle; Codepage: Integer);
+begin
+  SetLength(Items,Length(Items)+1);
+  Items[High(Items)].hDBEvent := hDBEvent;
+  Items[High(Items)].hContact := hContact;
+  Items[High(Items)].Codepage := Codepage;
+  Grid.Allocate(Length(Items));
+end;
+
+constructor TExternalGrid.Create(AParentWindow: HWND);
+begin
+  FParentWindow := AParentWindow;
+  Grid := THistoryGrid.CreateParented(ParentWindow);
+  Grid.OnItemData := GridItemData;
+  Grid.OnTranslateTime := GridTranslateTime;
+  Grid.Options := GridOptions;
+end;
+
+destructor TExternalGrid.Destroy;
+begin
+  Grid.Free;
+  Finalize(Items);
+  inherited;
+end;
+
+function TExternalGrid.GetGridHandle: HWND;
+begin
+  Result := Grid.Handle;
+end;
+
+procedure TExternalGrid.GridItemData(Sender: TObject; Index: Integer;
+  var Item: THistoryItem);
+begin
+  Item := ReadEvent(Items[Index].hDBEvent,Items[Index].Codepage);
+end;
+
+procedure TExternalGrid.GridTranslateTime(Sender: TObject; Time: Cardinal;
+  var Text: WideString);
+begin
+  Text := TimestampToString(Time);
+end;
+
+procedure TExternalGrid.ScrollToBottom;
+begin
+  Grid.MakeVisible(Length(Items)-1);
+end;
+
+procedure TExternalGrid.SetPosition(x, y, cx, cy: Integer);
+begin
+  Grid.Left := x;
+  Grid.Top := y;
+  Grid.Width := cx;
+  Grid.Height := cy;
+  SetWindowPos(Grid.Handle,0,x,y,cx,cy,SWP_SHOWWINDOW);
+end;
+
+initialization
+  SetLength(ExternalGrids,1);
+finalization
+  Finalize(ExternalGrids);
+end.
