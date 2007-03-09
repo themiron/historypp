a7f0f6c87731a9afd1be2e914dddd198ce9e4dd1
themiron <themiron@6986caf6-1fe9-11df-980b-c1ecf858a722>
2007-03-09 12:58:30 +0000

    + Version bump to 1.5.0.114 + Changelog updated

--- historypp/trunk/plugin/hpp_global.pas
+++ historypp/trunk/plugin/hpp_global.pas
@@ -96,7 +96,7 @@ const
   hppVerMajor   = {MAJOR_VER}1{/MAJOR_VER};
   hppVerMinor   = {MINOR_VER}5{/MINOR_VER};
   hppVerRelease = {SUB_VER}0{/SUB_VER};
-  hppVerBuild   = {BUILD}113{/BUILD};
+  hppVerBuild   = {BUILD}114{/BUILD};
   hppVerAlpha   = {$IFDEF ALPHA}True{$ELSE}False{$ENDIF};
   hppVersion    = hppVerMajor shl 24 + hppVerMinor shl 16 + hppVerRelease shl 8 + hppVerBuild;
 
