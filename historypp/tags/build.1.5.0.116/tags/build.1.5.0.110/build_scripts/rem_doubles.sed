335281d6216248638bda238ca5e35504615747c0
themiron@mail.ru <themiron@mail.ru@6986caf6-1fe9-11df-980b-c1ecf858a722>
2010-02-23 08:39:14 +0000

    Removed 1.5.0.116 megastuff

--- historypp/tags/build.1.5.0.116/tags/build.1.5.0.110/build_scripts/rem_doubles.sed
+++
@@ -1,5 +0,0 @@
-#http://www.rtfiber.com.tw/~changyj/sed/html/p.20001015a.html
-/^$/{
-N
-/^\n$/D
-}
--- historypp/tags/build.1.5.0.116/tags/build.1.5.0.114/plugin/historypp_icons.dpr
+++
@@ -1,7 +0,0 @@
-library historypp_icons;
-
-{$R 'historypp_icons.res' 'historypp_icons.rc'}
-
-begin
-
-end.
